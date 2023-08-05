6db97555afa1ddea6c7dc5a53a9fbf53a8b1786c
Killerhunter100 <88213307+Killerhunter100@users.noreply.github.com>
2023-08-05 19:03:09 +0300

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -238,7 +238,7 @@ RegisterNetEvent('qb-cityhall:client:sendDriverEmail', function(charinfo)
         if PlayerData.charinfo.gender == 1 then
             gender = Lang:t('email.mrs')
         end
-        TriggerServerEvent('qs-smartphone:server:sendNewMail', {
+        TriggerServerEvent('qb-phone:server:sendNewMail', {
             sender = Lang:t('email.sender'),
             subject = Lang:t('email.subject'),
             message =  Lang:t('email.message', {gender = gender, lastname = charinfo.lastname, firstname = charinfo.firstname, phone = charinfo.phone}),
